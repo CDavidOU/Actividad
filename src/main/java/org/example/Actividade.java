8dfa961233165d264f3c21e5fc4a6c8e8472ca97
Carlos Ortega <20253ds043@utez.edu.mx>
2026-05-22 15:03:55 -0600

    [New] segundo commit

--- src/main/java/org/example/Actividade.java
+++ src/main/java/org/example/Actividade.java
@@ -4,5 +4,9 @@ public class Actividade {
 
     public static void main(String[] args) {
         System.out.println("Hola como estas amigo");
+        System.out.println("Numero del 1 al 10");
+        for(int x=0;x<10;x++){
+            System.out.println("Numero"+x);
+        }
     }
 }
