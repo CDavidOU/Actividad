b0495d3864f599be31b7cc1179138043fc9648e4
Carlos Ortega <20253ds043@utez.edu.mx>
2026-05-25 07:35:43 -0600

    [NEW] segundo commit

--- src/main/java/org/example/Actividade.java
+++ src/main/java/org/example/Actividade.java
@@ -3,10 +3,12 @@ package org.example;
 public class Actividade {
 
     public static void main(String[] args) {
-        System.out.println("Hola como estas amigo");
+        System.out.println("Hola como estas amigo Carlos");
         System.out.println("Numero del 1 al 10");
         for(int x=0;x<10;x++){
-            System.out.println("Numero"+x);
+            System.out.println("Numero "+x);
         }
+
+        System.out.println("Es el segundo commit asi que adiooos");
     }
 }
